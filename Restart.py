789a728e7b8dddda5a46ca7e6033f1bdb26a6b9f
Fullmetal124 <122325756+Fullmetal124@users.noreply.github.com>
2023-02-13 18:37:48 -0700

    Create Restart.py
    restarts program, not done

---
+++ Restart.py
@@ -0,0 +1,7 @@
+def restrt()
+    rst = int(input("Do you want to go again?  1 = yes, 2= no"))
+    if rst == 1:
+        #do this once everything is compiled
+    if rst == 2:
+        print("Thanks for playing!")
+        

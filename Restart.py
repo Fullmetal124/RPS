d2d8b0cca8df0e38ce5ecd408fe1f33ab8477999
Fullmetal124 <122325756+Fullmetal124@users.noreply.github.com>
2023-02-15 17:17:05 -0700

    Delete Restart.py

--- Restart.py
+++
@@ -1,7 +0,0 @@
-def restrt()
-    rst = int(input("Do you want to go again?  1 = yes, 2= no"))
-    if rst == 1:
-        rockmenu()
-    if rst == 2:
-        print("Thanks for playing!")
-        
